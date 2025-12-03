8caa0154366f69d69fdbd1f129200658aa87e064
XOff2 <156075963+XOff2@users.noreply.github.com>
2025-12-03 20:36:26 +0330

    main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def save_data(data):
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=4)
 
+bot = commands.Bot(command_prefix="?", help_command=None, intents=intents)
 
 # ---------------- HELP COMMAND ----------------
 @bot.command()
